3f7d42854370fd10b4146cab6f6ec6560a943c48
adamgordon241 <102659680+adamgordon241@users.noreply.github.com>
2022-04-25 00:43:00 -0700

    init commit for avl code

---
+++ lang/en/qtype_avl.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_avl', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage avl
+ * @copyright  2022 adam gordon)
+
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'avl';
+$string['pluginname_help'] = 'Create a avlquestion type.';
+$string['pluginname_link'] = 'question/type/avl';
+$string['pluginnameadding'] = 'Adding an avl question';
+$string['pluginnameediting'] = 'Editing an avl question';
+$string['pluginnamesummary'] = 'An avl tree question type';
+
+$string["singleRotation"]="Perform single rotation";
+$string["doubleRotation"]="Perform double rotation";
+$string["addNode"]="Add a node to an avl tree";
+$string["removeNode"]="Remove a node from avl tree";
+$string["questionType"]="What type of avl question?";
+
+
