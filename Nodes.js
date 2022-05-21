bf4ebd234d52c61175154aebde7e5ed183fd6673
wasimrahiman <2327097@students.wits.ac.za>
2022-05-21 15:40:38 +0200

    Attemp 2 of improving coverage

--- Nodes.js
+++ Nodes.js
@@ -78,11 +78,6 @@ class Node {
         return this.RC;
     }
 
-
-
-
-
-
 }
 class Edge {
 
@@ -135,8 +130,6 @@ Added_Nodes = new Array();
 var Added_Edges = new Array();
 var subtree = new Array();
 var dummyNode = new Node(820, 620, -1, n);
-var previous_x = -1; //x coordinate of node that was selected previously
-var previous_y = -1; //y coordinate
 var IsSelected = false;
 var Node_ToMove;
 const Selected_Nodes = [-1, -1]; //tuple containing the nodes currently selected ,can have just one node selected or both
@@ -144,13 +137,6 @@ Added_Nodes.push(dummyNode);
 //addNode(820,620,-1)
 
 var Node_Selected;
-
-var drag_node;
-var count = 0;
-
-var rootexists = false;
-var numroots = 0;
-
 var x, y;
 
 
@@ -195,13 +181,6 @@ function installMouseHandler() {
         graphics.clearRect(0, 0, canvas.width, canvas.height);
         DrawAllEdges();
         DrawAllNodes();
-
-
-
-        //drag_node = Node_Selected;
-        //console.log("x" + Node_Selected.GetNodeNum())
-
-
         console.log("clicked on " + Node_Selected.GetNodeNum())
 
         if (Node_Selected.GetNodeNum() != -1 && Node_Selected.GetIs_Selected()) {
@@ -210,8 +189,6 @@ function installMouseHandler() {
         } else {
             closePopup();
         }
-
-
         // round to integer values; some browsers would give non-integers.
         // (this won't be the case for all mousedowns in all programs)
         if (dragging) {
@@ -222,9 +199,6 @@ function installMouseHandler() {
             document.addEventListener("mousemove", doMouseMove, false);
             document.addEventListener("mouseup", doMouseUp, false);
         }
-
-        //colorChoice = Number(document.getElementById("colorChoice").value);
-        // TODO: Anything else to do when mouse is first pressed?
     }
 
     function doMouseMove(evt) {
@@ -304,30 +278,10 @@ function addGraphicsContextExtras(graphics) {
         this.arc(x, y, radius, 0, 2 * Math.PI, false);
         this.stroke();
     }
-    graphics.fillPoly = function() {
-        if (arguments.length < 6)
-            return;
-        this.beginPath();
-        this.moveTo(arguments[0], arguments[1]);
-        for (var i = 2; i + 1 < arguments.length; i = i + 2) {
-            this.lineTo(arguments[i], arguments[i + 1]);
-        }
-        this.closePath();
-        this.fill();
-    }
-    graphics.getRGB = function(x, y) {
-        var color = this.getImageData(x, y, 1, 1);
-        return color.data;
-    }
 } // end of addGraphicsContextExtras()
 
 function DrawNewNode(Node, color) { //draws a new node
     //draws the white circle that represents a nodes
-    var Delx;
-    var Dely;
-    var Editx;
-    var Edity;
-
     graphics.save();
     graphics.fillStyle = "white";
     graphics.strokeStyle = color;
@@ -711,16 +665,7 @@ function editNode(num, nodenum, nodeindx) {
 
     }
 
-
-
-
-    for (let i = 0; i < Added_Nodes.length; i++) {
-        console.log(Added_Nodes[i].GetNodeNum(), "-->", Added_Nodes[i].GetArrIndex());
-    }
-    console.log("Edges are -->", Added_Edges);
-
     return (num);
-
 
 }
 
@@ -798,10 +743,6 @@ function init() {
     installMouseHandler();
     graphics.fillStyle = "white";
     graphics.fillRect(0, 0, canvas.width, canvas.height);
-
-    //document.getElementById("buttonAddNodeNumber").addEventListener("click",getVal);
-
-    //document.getElementById("canvas").onclick = OnCanvasClick;
     document.getElementById("canvas").addEventListener("dblclick", doNodeOperations);
     document.getElementById("canvas").addEventListener("contextmenu", doNodeOperations); // double click on the canvas to add a node
 }
